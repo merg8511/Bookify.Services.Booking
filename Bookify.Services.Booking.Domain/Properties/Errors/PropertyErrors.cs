22019d7489540f92bec50b2beb6e921514ebde3d
merg8511 <merg8511@gmail.com>
2026-07-17 11:41:14 -0600

    feat(properties): add semantic validation error definitions

---
+++ Bookify.Services.Booking.Domain/Properties/Errors/PropertyErrors.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Services.Booking.Domain.Shared;
+
+namespace Bookify.Services.Booking.Domain.Properties.Errors
+{
+    public static class PropertyErrors
+    {
+        public static readonly Error InvalidName = Error.Validation(
+            "Property.InvalidName",
+            "The property name cannot be empty");
+
+        public static readonly Error InvalidTimeZoneId = Error.Validation(
+            "Property.InvalidTimeZoneId",
+            "The property time zone identifier cannot be empty");
+    }
+}
