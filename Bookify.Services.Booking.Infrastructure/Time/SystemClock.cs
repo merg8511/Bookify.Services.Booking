e089c2d5f14d5dbb5e21dad06703fb1c939503ff
merg8511 <merg8511@gmail.com>
2026-07-20 16:49:41 -0600

    feat(infrastructure): implement SystemClock adapter based on IClock contract

---
+++ Bookify.Services.Booking.Infrastructure/Time/SystemClock.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Services.Booking.Application.Abstractions.Time;
+
+namespace Bookify.Services.Booking.Infrastructure.Time
+{
+    internal sealed class SystemClock : IClock
+    {
+        public DateTimeOffset UtcNow =>
+            DateTimeOffset.UtcNow;
+    }
+}
