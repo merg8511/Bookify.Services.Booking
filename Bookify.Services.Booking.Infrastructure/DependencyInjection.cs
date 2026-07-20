f811f75799a11c5ea9e3acb259b1799433de8896
merg8511 <merg8511@gmail.com>
2026-07-20 17:18:14 -0600

    feat(infrastructure): register SystemClock service implementation in AddInfrastructure

---
+++ Bookify.Services.Booking.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Services.Booking.Application.Abstractions.Time;
+using Bookify.Services.Booking.Infrastructure.Time;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Services.Booking.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services)
+        {
+            services.AddSingleton<IClock, SystemClock>();
+
+            return services;
+        }
+    }
+}
