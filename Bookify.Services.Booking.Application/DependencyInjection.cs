b008ca39bb4902c1adbb2d5fb9775c9082e5b6be
merg8511 <merg8511@gmail.com>
2026-07-20 17:18:07 -0600

    feat(application): add DependencyInjection extensions and abstractions package

---
+++ Bookify.Services.Booking.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Services.Booking.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
