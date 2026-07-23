d3e7ae04ade533c94c8900b33e9aacbaf4d90bfe
merg8511 <merg8511@gmail.com>
2026-07-23 12:25:31 -0600

    refactor(infrastructure): bind EF Core persistence in DI and remove in-memory store

--- Bookify.Services.Booking.Infrastructure/DependencyInjection.cs
+++ Bookify.Services.Booking.Infrastructure/DependencyInjection.cs
@@ -3,7 +3,7 @@ using Bookify.Services.Booking.Application.Abstractions.Persistence.Repositories
 using Bookify.Services.Booking.Application.Abstractions.Time;
 using Bookify.Services.Booking.Application.Properties.GetById;
 using Bookify.Services.Booking.Infrastructure.Persistence;
-using Bookify.Services.Booking.Infrastructure.Persistence.InMemory;
+using Bookify.Services.Booking.Infrastructure.Persistence.ReadServices;
 using Bookify.Services.Booking.Infrastructure.Persistence.Repositories;
 using Bookify.Services.Booking.Infrastructure.Time;
 using Microsoft.EntityFrameworkCore;
@@ -27,37 +27,39 @@ public static class DependencyInjection
                 options.UseNpgsql(
                     connectionString));
 
-        AddEfCoreRepositories(services);
-
-        AddTemporaryInMemoryPersistence(services);
+        AddPersistence(
+            services,
+            connectionString);
 
         return services;
     }
 
-    private static void AddEfCoreRepositories(
-        IServiceCollection services)
+    private static void AddPersistence(
+        IServiceCollection services,
+        string connectionString)
     {
-        services.AddScoped<PropertyRepository>();
-        services.AddScoped<RentableUnitRepository>();
-        services.AddScoped<BookingRepository>();
-    }
-
-    private static void AddTemporaryInMemoryPersistence(IServiceCollection services)
-    {
-        services.AddSingleton<InMemoryBookingStore>();
-
-        services.AddScoped<InMemoryUnitOfWork>();
+        services.AddDbContext<BookingDbContext>(
+            options =>
+                options.UseNpgsql(connectionString));
 
         services.AddScoped<IUnitOfWork>(
             serviceProvider =>
-                serviceProvider.GetRequiredService<InMemoryUnitOfWork>());
+                serviceProvider.GetRequiredService<BookingDbContext>());
 
         services.AddScoped<
             IPropertyRepository,
-            InMemoryPropertyRepository>();
+            PropertyRepository>();
+
+        services.AddScoped<
+            IRentableUnitRepository,
+            RentableUnitRepository>();
+
+        services.AddScoped<
+            IBookingRepository,
+            BookingRepository>();
 
         services.AddScoped<
             IPropertyReadService,
-            InMemoryPropertyReadService>();
+            EfCorePropertyReadService>();
     }
 }
