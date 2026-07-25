fe1d7441d10691458803cbab34ac9350a111d0b1
merg8511 <merg8511@gmail.com>
2026-07-24 19:19:32 -0600

    feat(api): add ProblemTypeUris constants for standardized problem types

---
+++ Bookify.Services.Booking.Api/Extensions/ProblemTypeUris.cs
@@ -0,0 +1,13 @@
+namespace Bookify.Services.Booking.Api.Extensions;
+
+internal static class ProblemTypeUris
+{
+    internal const string Validation =
+        "urn:bookify:problem-type:validation";
+
+    internal const string NotFound =
+        "urn:bookify:problem-type:not-found";
+
+    internal const string ServerError =
+        "urn:bookify:problem-type:server-error";
+}
