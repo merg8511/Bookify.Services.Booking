f4ff04ffe66452b4b0714761f440af4fe62ea960
merg8511 <merg8511@gmail.com>
2026-07-27 18:34:25 -0600

    feat(application): add common pagination abstractions and result models

---
+++ src/Bookify.Services.Booking.Application/Common/Pagination/PaginationDefaults.cs
@@ -0,0 +1,8 @@
+namespace Bookify.Services.Booking.Application.Common.Pagination;
+
+public static class PaginationDefaults
+{
+    public const int DefaultPageNumber = 1;
+    public const int DefaultPageSize = 20;
+    public const int MaximumPageSize = 100;
+}
