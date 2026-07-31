2420c794b85481c28ab3f185faf92900d2bd28fa
merg8511 <merg8511@gmail.com>
2026-07-31 10:02:08 -0600

    feat(infrastructure): implement dapper availability read service for overlapping bookings

--- src/Bookify.Services.Booking.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Services.Booking.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Bookify.Services.Booking.Application.Abstractions.Persistence;
 using Bookify.Services.Booking.Application.Abstractions.Persistence.Repositories;
 using Bookify.Services.Booking.Application.Abstractions.Time;
+using Bookify.Services.Booking.Application.Availability;
 using Bookify.Services.Booking.Application.Bookings;
 using Bookify.Services.Booking.Application.Properties;
 using Bookify.Services.Booking.Application.RentableUnits;
@@ -84,5 +85,9 @@ public static class DependencyInjection
         services.AddScoped<
             IBookingReadService,
             DapperBookingReadService>();
+
+        services.AddScoped<
+            IAvailabilityReadService,
+            DapperAvailabilityReadService>();
     }
 }
