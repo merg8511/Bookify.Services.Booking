86787c31155b92e56e6a6730eb39fba3652075b6
merg8511 <merg8511@gmail.com>
2026-08-12 18:18:26 -0600

    feat(domain): add extra guest pricing rule

--- src/Bookify.Services.Booking.Domain/Bookings/Services/BookingPricingEngine.cs
+++ src/Bookify.Services.Booking.Domain/Bookings/Services/BookingPricingEngine.cs
@@ -1,4 +1,5 @@
 using Bookify.Services.Booking.Domain.Bookings.ValueObjects;
+using Bookify.Services.Booking.Domain.Properties;
 using Bookify.Services.Booking.Domain.Shared;
 using Bookify.Services.Booking.Domain.Shared.ValueObjects;
 
@@ -14,5 +15,26 @@ public static class BookingPricingEngine
         ArgumentNullException.ThrowIfNull(stayPeriod);
 
         return nightlyRate.Multiply(stayPeriod.NumberOfNights);
+    }
+
+    public static Result<Money> CalculateExtraGuestPrice(
+        Money extraGuestNightlyRate,
+        RentableUnit rentableUnit,
+        GuestCount guestCount,
+        StayPeriod stayPeriod)
+    {
+        ArgumentNullException.ThrowIfNull(extraGuestNightlyRate);
+        ArgumentNullException.ThrowIfNull(rentableUnit);
+        ArgumentNullException.ThrowIfNull(guestCount);
+        ArgumentNullException.ThrowIfNull(stayPeriod);
+
+        int extraGuestCount =
+            Math.Max(
+                0,
+                guestCount.Value - rentableUnit.MaxBaseGuests);
+
+        int extraGuestNights = extraGuestCount * stayPeriod.NumberOfNights;
+
+        return extraGuestNightlyRate.Multiply(extraGuestNights);
     }
 }
